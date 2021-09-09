228d065f41c9ba86558bdf6327b040277c536703
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-09 21:10:56 +0200

    test

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App extends Component {
     
     calculateFaceLocation = (data) => {
       data.outputs[0].data.regions.forEach( data => {
+        for(let i=0; i<data.outputs[0].data.regions.length; i++){
         const clarifaiFace = data.outputs[0].data.regions[i].region_info.bounding_box;
         const image = document.getElementById('inputimage');
         const width = Number(image.width);
@@ -52,7 +53,8 @@ class App extends Component {
           topRow: clarifaiFace.top_row * height,
           rightCol: width - (clarifaiFace.right_col * width),
           bottomRow: height - (clarifaiFace.bottom_row * height), 
-          } }
+          
+          } }}
         ) 
       }
       
