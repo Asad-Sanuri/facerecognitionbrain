b2d05fed8cb59dd401c44d15e77cab2216025261
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-11 00:05:28 +0200

    test

--- src/App.js
+++ src/App.js
@@ -55,11 +55,12 @@ class App extends Component {
         const width = Number(image.width);
         const height = Number(image.height);
                               
-        return{               
+        return{
+          bottomRow: height - (clarifaiFace.bottom_row * height),               
           leftCol: clarifaiFace.left_col * width,
-          topRow: clarifaiFace.top_row * height,
           rightCol: width - (clarifaiFace.right_col * width),
-          bottomRow: height - (clarifaiFace.bottom_row * height),
+          topRow: clarifaiFace.top_row * height,
+                    
                 }  
               })                      
             }           
