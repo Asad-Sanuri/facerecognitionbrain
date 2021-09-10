9ad5d08cfc7f40ca5f42b4977f37e049e1c1a4a7
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-11 00:33:49 +0200

    test

--- src/App.js
+++ src/App.js
@@ -43,13 +43,12 @@ class App extends Component {
     
     calculateFaceLocation = (data) => {        
       //const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
-      data.outputs[0].data.regions.forEach( function(region.region_info.bounding_box){
+      data.outputs[0].data.regions.forEach( function(region){
          console.log(region.region_info.bounding_box); //} )         
-        const clarifaiFace = region.region_info.bounding_box; 
+        let clarifaiFace = region.region_info.bounding_box; 
         const image = document.getElementById('inputimage');
         const width = Number(image.width);
         const height = Number(image.height);
-
         return{
           bottomRow: height - (clarifaiFace.bottom_row * height),               
           leftCol: clarifaiFace.left_col * width,
@@ -64,7 +63,7 @@ class App extends Component {
           
        
     displayFaceBox = (box) => {
-        this.setState({box: box});
+        this.setState( [ {box: box}, {box: box} ] );
     }
     
     onInputChange = (event) => {
