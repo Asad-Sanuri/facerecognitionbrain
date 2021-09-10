63394798755e349170b44ef57dbfe994872f90d1
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-10 22:55:25 +0200

    test

--- src/App.js
+++ src/App.js
@@ -50,16 +50,14 @@ class App extends Component {
         const image = document.getElementById('inputimage');
         const width = Number(image.width);
         const height = Number(image.height);
-        
-        
+               
         return{               
           leftCol: clarifaiFace.left_col * width,
           topRow: clarifaiFace.top_row * height,
           rightCol: width - (clarifaiFace.right_col * width),
           bottomRow: height - (clarifaiFace.bottom_row * height),
-          bobo: console.log('first one', leftCol, topRow, rightCol, bottomRow),             
-            }
-            
+                      
+            }           
           })      
           }
         
