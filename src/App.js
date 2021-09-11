c389e23384f87063de2356c3df0ef8a228622cdd
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-11 04:25:00 +0200

    test

--- src/App.js
+++ src/App.js
@@ -42,13 +42,12 @@ class App extends Component {
     }
     
     calculateFaceLocation = (data) => {
-      //OG const clarifaiFace = data.outputs[0].data.regions[0,1].region_info.bounding_box;  
+      // OG const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;  
       //data.outputs[0].data.regions.forEach(function(region){
-        //const clarifaiFace = data.outputs[0].data.regions[0,1].region_info.bounding_box;      
-             
-      data.outputs[0].data.regions.forEach(function(region){
-        console.log(region.region_info.bounding_box);                    
-        const clarifaiFace = region[0].region_info.bounding_box; 
+        //const clarifaiFace = data.outputs[0].data.regions[0,1].region_info.bounding_box;
+      data.outputs[0].data.regions.forEach(function(region){      
+        console.log(region[0, 1].region_info.bounding_box);                    
+        const clarifaiFace = region[0, 1].region_info.bounding_box; 
         const image = document.getElementById('inputimage');
         const width = Number(image.width);
         const height = Number(image.height);
@@ -56,7 +55,7 @@ class App extends Component {
           bottomRow: height - (clarifaiFace.bottom_row * height),               
           leftCol: clarifaiFace.left_col * width,
           rightCol: width - (clarifaiFace.right_col * width),
-          topRow: clarifaiFace.top_row * height
+          topRow: clarifaiFace.top_row * height,
                 }
               }
             )
