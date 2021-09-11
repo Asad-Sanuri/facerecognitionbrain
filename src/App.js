321ebda312d9f6490b7c804d3d4bffdb052f5f04
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-11 19:37:55 +0200

    test

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ class App extends Component {
       for(let j=i-1; j<data.outputs[0].data.regions.length; j++){
         console.log('i=' , i, 'j=', j);
         console.log(data.outputs[0].data.regions[j].region_info.bounding_box);   */
-      for(let i=0; i<data.outputs[0].data.regions.length; i++){
-        let clarifaiFace = [];                  
+        let clarifaiFace = [];  
+      for(let i=0; i<data.outputs[0].data.regions.length; i++){                        
         clarifaiFace[i] = data.outputs[0].data.regions[i].region_info.bounding_box;          
         const image = document.getElementById('inputimage');
         const width = Number(image.width);
