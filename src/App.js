a49183ec2bea74e8caa48d92f7edaaa51b4c4242
Asad-Sanuri <asad.sanuri@gmail.com>
2021-09-16 14:20:49 +0200

    test

--- src/App.js
+++ src/App.js
@@ -59,25 +59,24 @@ class App extends Component {
         for(let j=i-1; j<data.outputs[0].data.regions.length; j++){
           console.log('i=' , i, 'j=', j);
           console.log(data.outputs[0].data.regions[j].region_info.bounding_box);  
-          console.log(jer);
-          //let clarifaiFace = [];                                   
-          const clarifaiFace = data.outputs[0].data.regions[j].region_info.bounding_box;          
+          
+          const clarifaiFace = [];                                   
+            clarifaiFace = data.outputs[0].data.regions[j].region_info.bounding_box;          
           const image = document.getElementById('inputimage');
           const width = Number(image.width);
           const height = Number(image.height);        
           
           return{
             jer: j,                                               
-            leftCol: clarifaiFace.left_col * width,
-            topRow: clarifaiFace.top_row * height,
-            rightCol: width - (clarifaiFace.right_col * width),
-            bottomRow: height - (clarifaiFace.bottom_row * height)                  
+            leftCol: clarifaiFace[j].left_col * width,
+            topRow: clarifaiFace[j].top_row * height,
+            rightCol: width - (clarifaiFace[j].right_col * width),
+            bottomRow: height - (clarifaiFace[j].bottom_row * height)                  
                       }
                     }                    
                   }
                   bar();
-                }
-                console.log("final", jer);
+                }                
               }                
                                                                         
     displayFaceBox = (box, jer) => {
