b46e7f38e6865270d38cb45fa7cfd3e00dd8b50f
Asad-Sanuri <asad.sanuri@gmail.com>
2021-10-07 11:59:26 +0200

    test

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,11 +8,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, input}) => {
                 {'Upload an image or put an image url-address to detect faces.'} 
             </p>
             <div className='center'>               
-                <div className='form center pa4 br3 shadow-5'>                    
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} /> 
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple' disabled={!input} onClick = {onButtonSubmit}>Detect</button>
+                <div className='form center pa4 br3 shadow-5'>
                     <label className="f3 white">Choose a file </label>
-                    <input type="file" id="image" name="image" accept="image/*" onChange={onInputChange} />
+                    <input type="file" id="image" name="image" accept="image/*" onChange={onInputChange} />                    
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} /> 
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple' disabled={!input} onClick = {onButtonSubmit}>Detect</button>                    
                 </div>
             </div>
         </div>
