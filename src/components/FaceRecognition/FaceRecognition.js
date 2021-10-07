46297b91a3b79064f9bbe32547a38254477774b1
Asad-Sanuri <asad.sanuri@gmail.com>
2021-10-07 12:11:28 +0200

    test

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -7,7 +7,7 @@ const FaceRecognition = ({ imageUrl, boxes} ) => {
             <div className='absolute mt2'>
                 <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />                 
                 {boxes.map(box =>{
-                    return <div className='bounding-box'
+                    return <div /* key={box.topRow} */ className='bounding-box'
                     style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
                 })
                 }                                               
